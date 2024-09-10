429cdb050830d17f98585c67f841668016c113e5
pabloduarte9679 <elierduarte344@gmail.com>
2024-09-09 20:55:56 -0600

    inital commit

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <dirent.h>
+#include <stdio.h>
+
+int main(void){
+  DIR *directory;
+  struct dirent *entry;
+  directory = opendir(".");
+  if(directory == NULL){
+    printf("Error opening directory\n");
+    return 1;
+  }
+  while((entry = readdir(directory)) != NULL){
+    if(entry->d_type == DT_REG){
+      printf("File: %s\n", entry->d_name);
+    }else if(entry->d_type == DT_DIR){
+      printf("Dir: %s\n", entry->d_name);
+    }
+  }
+  if(closedir(directory) == -1){
+    printf("Error closing directory\n");
+    return 1;
+  }
+  return 0;
+}
